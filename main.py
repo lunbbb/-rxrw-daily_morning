6230ee8aed84c4eeb1bc1bffe33f86e1ae8dd626
lunbbb <125731855+lunbbb@users.noreply.github.com>
2023-02-18 19:55:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ template_id = os.environ["TEMPLATE_ID"]
 def get_weather():
   url = "https://v0.yiketianqi.com/api?unescape=1&version=v91&appid=43656176&appsecret=I42og6Lm&ext=&cityid=&city=" + city
   res = requests.get(url).json()
-  weather = res['data']['list'][0]
+  weather = res['data'][0]['list']
   return weather['weather'], math.floor(weather['temp'])
 
 def get_count():
